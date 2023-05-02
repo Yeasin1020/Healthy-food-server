55c0e9c823e4aea5d70e7fed9a9e909cd5ad8a44
Yeasin1st <ysbd111@gmail.com>
2023-05-02 11:06:48 +0600

    api add client site add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json')
 
 app.use(cors());
 
@@ -10,7 +11,9 @@ app.get('/', (req, res) => {
 	res.send('Dragon is running')
 });
 
-
+app.get('/categories', (req, res)=>{
+	res.send(categories);
+})
 
 app.listen(port, () => {
 	console.log(`Dragon API is running on port: ${port}`)
