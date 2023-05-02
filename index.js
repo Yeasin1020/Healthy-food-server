0cdb915945bc0c0a0f4f30abb2d9b16cadd54a06
Yeasin1st <ysbd111@gmail.com>
2023-05-02 11:05:46 +0600

    nodemon

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('Dragon is running')
+});
+
+
+
+app.listen(port, () => {
+	console.log(`Dragon API is running on port: ${port}`)
+})
